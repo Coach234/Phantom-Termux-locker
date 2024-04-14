2e7bf95dd1d58994b8b7d65fa96e505d90dfb30d
Coach234 <135114541+Coach234@users.noreply.github.com>
2024-04-14 11:15:42 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 
-# Setup and activation of Termux-Lock
+# Setup and activation of Phantom-Termux-Locker
 
 import os,sys
 
 os.chdir('/data/data/com.termux/files/usr/etc')
 
 opr = open('bash.bashrc','a')
-opr.write("\nalias txl='python /data/data/com.termux/files/home/Termux-Lock/Termux-Lock.py -l'\n")
+opr.write("\nalias txl='python /data/data/com.termux/files/home/Phantom-Termux-Locker/Phantom-Termux-Locker.py -l'\n")
 opr.write('txl\n')
 opr.close()
 
